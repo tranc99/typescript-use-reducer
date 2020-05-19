946cc7d39c463bec6f0b79371ad7498d258397a5
Ten <nomadhideout@yandex.com>
2020-05-19 04:06:40 +0200

    add useState functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -16,31 +16,60 @@ const menuItems = [
     waitTime: '10 mins',
     price: '$40',
     imgURL: 'https://cdn.pixabay.com/photo/2016/03/05/19/02/abstract-1238247_960_720.jpg'
+  },
+  {
+    itemType: 'shrimp',
+    name: `Crabby's Shrimp Platter`,
+    waitTime: '15 mins',
+    price: '$50',
+    imgURL: 'https://cdn.pixabay.com/photo/2012/07/27/20/48/prawns-53204_960_720.jpg'
   }
 ]
 
-let menuOptions = menuItems.map((item) => {
+const menuOptions = menuItems.map((item) => {
   return (<option value={item.itemType}>{item.name}</option>)
 })
 
+const initialState = menuItems[0]
+
 function App() {
+
+  const [selectedItem, setSelectedItem] = useState(initialState)
+
+  const handleChange = (e: any) => {
+    alert(e.target.value)
+    let newSelection = menuItems.find(item => item.itemType === e.target.value)
+    if (newSelection !== undefined) {
+      setSelectedItem(newSelection)
+    }
+  }
+
+  const handleOrder = () => {
+    alert('Ordering ' + selectedItem.name + ' for ' + selectedItem.price)
+    // send order to our server
+  }
 
   return (
     <div className="App">
       <h1>The Crabby Fish 🍔</h1>
       <h2>Welcome to The Crabby Fish.</h2>
       <h3> Please select your entree below.</h3>
-      <select onChange={e => alert(e.target.value)}>
+      <select onChange={handleChange}>
         {menuOptions}
       </select>
-      <button>Place Order</button>
-      <div>
-        Review your order:
-        <h3>{menuItems[1].name}</h3>
-        <h4>Price - {menuItems[1].price}</h4>
-        <h4>Ready in {menuItems[1].waitTime}</h4>
-        <img src={menuItems[1].imgURL} alt="dish" width="300" height="300" />
-      </div>
+      <button
+        onClick={handleOrder}>
+        Place Order
+      </button>
+          <div>
+          Review your order:
+          <h3>{selectedItem.name}</h3>
+          <h4>Price - {selectedItem.price}</h4>
+          <h4>Ready in {selectedItem.waitTime}</h4>
+          <img src={selectedItem.imgURL} alt="your selection" width="300" height="300" />
+          </div>
+      }
+
     </div>
   );
 }
