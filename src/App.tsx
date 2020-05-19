e079345565cd4ed5c5498721b641547978a4b7f2
Ten <nomadhideout@yandex.com>
2020-05-19 02:47:33 +0200

    add basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const menuItems = [
+    {
+      itemType: 'pizza',
+      name: 'The Crabby Pizza',
+      waitTime: '20 mins',
+      price: '$30',
+      imgURL: 'https://cdn.pixabay.com/photo/2018/04/07/15/03/pizza-3298685_960_720.jpg'
+    },
+    {
+      itemType: 'burger',
+      name: 'The Famous Crabby Burger',
+      waitTime: '10 mins',
+      price: '$40',
+      imgURL: 'https://cdn.pixabay.com/photo/2016/03/05/19/02/abstract-1238247_960_720.jpg'
+    }
+  ]
+
+  let menuOptions = menuItems.map((item) => {
+    return (<option value={item.itemType}>{item.name}</option>)
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The Crabby Fish 🍔</h1>
+      <h2>Welcome to The Crabby Fish.</h2>
+      <h3> Please select your entree below.</h3>
+      <select onChange={e => alert(e.target.value)}>
+        {menuOptions}
+      </select>
+      <button>Place Order</button>
+      <div>
+        Review your order:
+        <h3>{menuItems[1].name}</h3>
+        <h4>Price - {menuItems[1].price}</h4>
+        <h4>Ready in {menuItems[1].waitTime}</h4>
+        <img src={menuItems[1].imgURL} alt="dish" width="300" height="300" />
+      </div>
     </div>
   );
 }
