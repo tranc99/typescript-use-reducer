ea6b694e66cc1eaab84c1e2e22a8679a4410fb87
Ten <nomadhideout@yandex.com>
2020-05-19 04:46:08 +0200

    add two side by side examples

--- src/App_UseState.tsx
+++ src/App_UseState.tsx
@@ -1,4 +1,6 @@
-import React, { useReducer } from 'react';
+// This is a version of our App component that uses the useState hook
+// instead of the useReducer hoo. The UI interaction effect is similar.
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,7 +8,6 @@ const menuItems = [
   {
     itemType: 'pizza',
     name: 'The Crabby Pizza',
-    description: 'Our original Crabby Pizza made by Chef John.',
     waitTime: '30 mins',
     price: '$30',
     imgURL: 'https://cdn.pixabay.com/photo/2018/04/07/15/03/pizza-3298685_960_720.jpg'
@@ -14,7 +15,6 @@ const menuItems = [
   {
     itemType: 'burger',
     name: 'The Famous Crabby Burger',
-    description: 'The burger that made history.',
     waitTime: '10 mins',
     price: '$40',
     imgURL: 'https://cdn.pixabay.com/photo/2016/03/05/19/02/abstract-1238247_960_720.jpg'
@@ -22,7 +22,6 @@ const menuItems = [
   {
     itemType: 'shrimp',
     name: `Crabby's Shrimp Platter`,
-    description: 'Healthy and sumptious, the king of shrimp.',
     waitTime: '15 mins',
     price: '$50',
     imgURL: 'https://cdn.pixabay.com/photo/2012/07/27/20/48/prawns-53204_960_720.jpg'
@@ -33,23 +32,22 @@ const menuOptions = menuItems.map((item) => {
   return (<option value={item.itemType}>{item.name}</option>)
 })
 
-const initialState = {selectedItem: menuItems[0]}
-
-function reducer(state: any, action: any) {
-  let newSelection = menuItems.find(item => item.itemType === action.type)
-  if (newSelection !== undefined) {
-    return {selectedItem: newSelection}
-  } else {
-    return state
-  }
-}
+const initialState = menuItems[0]
 
 function App() {
 
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [selectedItem, setSelectedItem] = useState(initialState)
+
+  const handleChange = (e: any) => {
+    // alert(e.target.value)
+    let newSelection = menuItems.find(item => item.itemType === e.target.value)
+    if (newSelection !== undefined) {
+      setSelectedItem(newSelection)
+    }
+  }
 
   const handleOrder = () => {
-    alert('Ordering ' + state.selectedItem.name + ' for ' + state.selectedItem.price)
+    alert('Ordering ' + selectedItem.name + ' for ' + selectedItem.price)
     // send order to our server
   }
 
@@ -58,21 +56,22 @@ function App() {
       <h1>The Crabby Fish 🍔</h1>
       <h2>Welcome to The Crabby Fish.</h2>
       <h3> Please select your entree below.</h3>
-      <select onChange={(e) => dispatch({type: e.target.value})}>
+      <select onChange={handleChange}>
         {menuOptions}
       </select>
       <button
         onClick={handleOrder}>
         Place Order
       </button>
-      <div>
-        Review your order:
-        <h3>{state.selectedItem.name}</h3>
-        <h4><em>{state.selectedItem.description}</em></h4>
-        <h4>Price - {state.selectedItem.price}</h4>
-        <h4>Ready in {state.selectedItem.waitTime}</h4>
-        <img src={state.selectedItem.imgURL} alt="your selection" width="300" height="300" />
-      </div>
+          <div>
+          Review your order:
+          <h3>{selectedItem.name}</h3>
+          <h4>Price - {selectedItem.price}</h4>
+          <h4>Ready in {selectedItem.waitTime}</h4>
+          <img src={selectedItem.imgURL} alt="your selection" width="300" height="300" />
+          </div>
+      }
+
     </div>
   );
 }
